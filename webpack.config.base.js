ab9257e2c752773b221978ada6aa3167b9d618ac
ux-builder <sdh485@gmail.com>
2026-01-18 03:37:46 +0900

    refactor: 환경변수 및 시크릿 이름 MM_ → OKRBEST_ 리브랜드
    - webpack.config.base.js: 빌드 환경변수 리브랜드
    - e2e/modules/environment.js: 테스트 환경변수 리브랜드
    - nightly-rainforest.yml: 빌드 환경변수 리브랜드
    - e2e-functional-template.yml: E2E 테스트 환경변수/시크릿 리브랜드
    - e2e-functional.yml: E2E 테스트 입력 파라미터 리브랜드
    - compatibility-matrix-testing.yml: CMT 입력 파라미터 리브랜드

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -19,9 +19,9 @@ const codeDefinitions = {
     __CAN_UPGRADE__: isTest || JSON.stringify(process.env.CAN_UPGRADE === 'true'),
     __IS_NIGHTLY_BUILD__: JSON.stringify(process.env.GITHUB_WORKFLOW && process.env.GITHUB_WORKFLOW.startsWith('nightly')),
     __IS_MAC_APP_STORE__: JSON.stringify(process.env.IS_MAC_APP_STORE === 'true'),
-    __SKIP_ONBOARDING_SCREENS__: JSON.stringify(process.env.MM_DESKTOP_BUILD_SKIPONBOARDINGSCREENS === 'true'),
-    __DISABLE_GPU__: JSON.stringify(process.env.MM_DESKTOP_BUILD_DISABLEGPU === 'true'),
-    __SENTRY_DSN__: JSON.stringify(process.env.MM_DESKTOP_BUILD_SENTRYDSN || ''),
+    __SKIP_ONBOARDING_SCREENS__: JSON.stringify(process.env.OKRBEST_DESKTOP_BUILD_SKIPONBOARDINGSCREENS === 'true'),
+    __DISABLE_GPU__: JSON.stringify(process.env.OKRBEST_DESKTOP_BUILD_DISABLEGPU === 'true'),
+    __SENTRY_DSN__: JSON.stringify(process.env.OKRBEST_DESKTOP_BUILD_SENTRYDSN || ''),
 };
 codeDefinitions['process.env.NODE_ENV'] = JSON.stringify(process.env.NODE_ENV);
 if (isTest) {
