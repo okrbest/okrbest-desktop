mattermost/desktop

[MM-46859] Refactor RegistryConfig into policyConfigLoader, include macOS CFPref support (#3692)
* [MM-46859] Refactor RegistryConfig into policyConfigLoader, include macOS CFPref support

* Fix e2e

src/common/config/policyConfigLoader.test.js
@@ -1,0 +1,232 @@
+// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {getPreferenceValue as getCFPreferenceValue} from 'cf-prefs';
+import {enumerateValues} from 'registry-js';
+
+import policyConfigLoader from 'common/config/policyConfigLoader';
+
+jest.mock('registry-js', () => {
+    return {
+        HKEY: {
+            HKEY_LOCAL_MACHINE: 'HKEY_LOCAL_MACHINE',
+            HKEY_CURRENT_USER: 'HKEY_CURRENT_USER',
+        },
+        enumerateValues: jest.fn().mockImplementation((hive, key) => {
+            if (hive === 'HKEY_LOCAL_MACHINE') {
+                if (key.endsWith('DefaultServerList')) {
+                    return [
+                        {name: 'server-lm-1', data: 'http://server-lm-1.com'},
+                        {name: 'server-lm-2', data: 'http://server-lm-2.com'},
+                    ];
+                }
+                if (key.includes('SOFTWARE\\Policies\\Mattermost')) {
+                    return [
+                        {name: 'EnableServerManagement', data: 1},
+                        {name: 'EnableAutoUpdater', data: 0},
+                    ];
+                }
+                return [];
+            }
+            if (hive === 'HKEY_CURRENT_USER') {
+                if (key.endsWith('DefaultServerList')) {
+                    return [
+                        {name: 'server-cu-1', data: 'http://server-cu-1.com'},
+                        {name: 'server-cu-2', data: 'http://server-cu-2.com'},
+                    ];
+                }
+                if (key.includes('SOFTWARE\\Policies\\Mattermost')) {
+                    return [
+                        {name: 'EnableServerManagement', data: 0},
+                        {name: 'EnableAutoUpdater', data: 1},
+                    ];
+                }
+                return [];
+            }
+            if (key.includes('Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize')) {
+                return [{name: 'AppsUseLightTheme', data: 1}];
+            }
+            return [];
+        }),
+    };
+});
+
+jest.mock('cf-prefs', () => ({
+    getPreferenceValue: jest.fn().mockImplementation((key) => {
+        if (key === 'DefaultServerList') {
+            return [
+                {name: 'server-a', url: 'https://a.com'},
+                {name: 'server-b', url: 'https://b.com'},
+            ];
+        }
+        if (key === 'EnableServerManagement') {
+            return true;
+        }
+        if (key === 'EnableAutoUpdater') {
+            return false;
+        }
+        return undefined;
+    }),
+}));
+
+describe('common/config/policyConfigLoader', () => {
+    let originalPlatform;
+
+    beforeEach(() => {
+        originalPlatform = process.platform;
+    });
+
+    afterEach(() => {
+        Object.defineProperty(process, 'platform', {value: originalPlatform});
+    });
+
+    describe('getPolicyConfig', () => {
+        describe('Windows', () => {
+            beforeEach(() => {
+                Object.defineProperty(process, 'platform', {value: 'win32'});
+            });
+
+            it('returns merged servers from both hives and CU overrides LM for booleans', () => {
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers).toHaveLength(4);
+                expect(data.servers).toContainEqual({name: 'server-lm-1', url: 'http://server-lm-1.com'});
+                expect(data.servers).toContainEqual({name: 'server-cu-1', url: 'http://server-cu-1.com'});
+                expect(data.enableServerManagement).toBe(false);
+                expect(data.enableUpdateNotifications).toBe(true);
+            });
+
+            it('handles undefined from one hive', () => {
+                enumerateValues.mockImplementation((hive, key) => {
+                    if (key.includes('Mattermost') && !key.includes('DefaultServerList')) {
+                        return hive === 'HKEY_LOCAL_MACHINE' ? [{name: 'EnableServerManagement', data: 1}] : [];
+                    }
+                    return [];
+                });
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.enableServerManagement).toBe(true);
+            });
+
+            it('handles registry error in one hive', () => {
+                enumerateValues.mockImplementation((hive, key) => {
+                    if (key.includes('Mattermost') && hive === 'HKEY_CURRENT_USER') {
+                        throw new Error('Registry access error');
+                    }
+                    if (key.includes('Mattermost') && !key.includes('DefaultServerList')) {
+                        return [{name: 'EnableServerManagement', data: 1}];
+                    }
+                    if (key.endsWith('DefaultServerList')) {
+                        return hive === 'HKEY_LOCAL_MACHINE' ? [{name: 's', data: 'http://s.com'}] : [];
+                    }
+                    return [];
+                });
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.enableServerManagement).toBe(true);
+                expect(data.servers).toHaveLength(1);
+            });
+
+            it('handles malformed server data', () => {
+                enumerateValues.mockImplementation((hive, key) => {
+                    if (key.endsWith('DefaultServerList')) {
+                        return [
+                            {name: 'server-1', data: null},
+                            {name: 'server-2', data: 'http://server-2.com'},
+                        ];
+                    }
+                    return [];
+                });
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers.length).toBeGreaterThanOrEqual(2);
+                expect(data.servers[0]).toEqual({name: 'server-1', url: null});
+                expect(data.servers[1]).toEqual({name: 'server-2', url: 'http://server-2.com'});
+            });
+
+            it('returns consistent data from multiple calls', () => {
+                const first = policyConfigLoader.getPolicyConfig();
+                const second = policyConfigLoader.getPolicyConfig();
+                expect(second.servers).toHaveLength(first.servers.length);
+                expect(second.enableServerManagement).toBe(first.enableServerManagement);
+            });
+        });
+
+        describe('macOS', () => {
+            beforeEach(() => {
+                Object.defineProperty(process, 'platform', {value: 'darwin'});
+            });
+
+            it('returns servers and booleans from managed preferences', () => {
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers).toEqual([
+                    {name: 'server-a', url: 'https://a.com'},
+                    {name: 'server-b', url: 'https://b.com'},
+                ]);
+                expect(data.enableServerManagement).toBe(true);
+                expect(data.enableUpdateNotifications).toBe(false);
+            });
+
+            it('returns empty servers and undefined booleans when no managed prefs', () => {
+                getCFPreferenceValue.mockImplementation(() => undefined);
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers).toEqual([]);
+                expect(data.enableServerManagement).toBeUndefined();
+                expect(data.enableUpdateNotifications).toBeUndefined();
+            });
+
+            it('handles cf-prefs throw gracefully', () => {
+                getCFPreferenceValue.mockImplementation((key) => {
+                    if (key === 'DefaultServerList') {
+                        throw new Error('cf-prefs error');
+                    }
+                    return undefined;
+                });
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers).toEqual([]);
+            });
+        });
+    });
+
+    describe('getAppsUseLightTheme', () => {
+        it('returns true when AppsUseLightTheme is 1 on Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'win32'});
+            enumerateValues.mockImplementation((hive, key) => {
+                if (key.includes('Themes\\Personalize')) {
+                    return [{name: 'AppsUseLightTheme', data: 1}];
+                }
+                return [];
+            });
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(true);
+        });
+
+        it('returns false when AppsUseLightTheme is 0 on Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'win32'});
+            enumerateValues.mockImplementation((hive, key) => {
+                if (key.includes('Themes\\Personalize')) {
+                    return [{name: 'AppsUseLightTheme', data: 0}];
+                }
+                return [];
+            });
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(false);
+        });
+
+        it('returns true when registry key missing on Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'win32'});
+            enumerateValues.mockImplementation(() => []);
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(true);
+        });
+
+        it('returns true on non-Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'darwin'});
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(true);
+        });
+
+        it('returns true when registry access throws on Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'win32'});
+            enumerateValues.mockImplementation((hive, key) => {
+                if (key.includes('Themes\\Personalize')) {
+                    throw new Error('Registry error');
+                }
+                return [];
+            });
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(true);
+        });
+    });
+});

src/common/config/policyConfigLoader.ts
@@ -1,0 +1,112 @@
+// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import type {CFPrefValue} from 'cf-prefs';
+import {getPreferenceValue as getCFPreferenceValue} from 'cf-prefs';
+import type {RegistryValue} from 'registry-js';
+import {HKEY, enumerateValues} from 'registry-js';
+
+import {Logger} from 'common/log';
+
+import type {RegistryConfig as RegistryConfigType, Server} from 'types/config';
+
+const log = new Logger('PolicyConfigLoader');
+const WINDOWS_REGISTRY_PATH = 'SOFTWARE\\Policies\\Mattermost';
+const WINDOWS_REGISTRY_HIVE_LIST = [HKEY.HKEY_LOCAL_MACHINE, HKEY.HKEY_CURRENT_USER];
+
+export class PolicyConfigLoader {
+    getPolicyConfig = (): Partial<RegistryConfigType> => {
+        return {
+            servers: this.getServerList(),
+            enableServerManagement: this.getSingleBooleanValue('EnableServerManagement'),
+            enableUpdateNotifications: this.getSingleBooleanValue('EnableAutoUpdater'),
+        };
+    };
+
+    getAppsUseLightTheme = (): boolean => {
+        if (process.platform !== 'win32') {
+            return true;
+        }
+        try {
+            const results = enumerateValues(HKEY.HKEY_CURRENT_USER, 'Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize');
+            if (!results || results.length === 0) {
+                return true;
+            }
+            return results.find((item) => item.name === 'AppsUseLightTheme')?.data === 1;
+        } catch (error) {
+            log.debug('Error reading AppsUseLightTheme from registry', error);
+            return true;
+        }
+    };
+
+    private getServerList = (): Server[] => {
+        switch (process.platform) {
+        case 'win32':
+            return this.getWindowsServerList();
+        case 'darwin':
+            return this.getMacOSServerList();
+        default:
+            return [];
+        }
+    };
+
+    private getWindowsServerList = (): Server[] => {
+        const results = this.getWindowsValues('DefaultServerList');
+        return results.map((item) => ({name: item.name, url: item.data as string}));
+    };
+
+    private getMacOSServerList = (): Server[] => {
+        const results = this.getMacOSValue('DefaultServerList') as Array<Record<string, string>> | undefined;
+        return (results ?? []).map((item) => ({name: item.name, url: item.url}));
+    };
+
+    private getSingleBooleanValue = (valueName: string): boolean | undefined => {
+        switch (process.platform) {
+        case 'win32':
+            return this.getWindowsSingleBooleanValue(valueName);
+        case 'darwin':
+            return this.getMacOSSingleValue(valueName) as boolean | undefined;
+        default:
+            return false;
+        }
+    };
+
+    // Windows registry values are always numbers or strings, so we just check if the value is 1.
+    private getWindowsSingleBooleanValue = (valueName: string): boolean | undefined => {
+        const value = this.getWindowsSingleValue(valueName);
+        return value === undefined ? undefined : value === 1;
+    };
+
+    private getMacOSSingleValue = (valueName: string): Exclude<CFPrefValue, CFPrefValue[]> | undefined =>
+        this.getMacOSValue(valueName) as Exclude<CFPrefValue, CFPrefValue[]>;
+    private getWindowsSingleValue = (valueName: string) => {
+        // Windows provides 2 hives, so we need to get the value from both and return the first one
+        // The first one is the local machine hive, which should take priority
+        const results = this.getWindowsValues()?.filter((r) => r.name === valueName);
+        return results?.pop()?.data;
+    };
+
+    private getWindowsValues = (key?: string) => {
+        const results: RegistryValue[] = [];
+        for (const hive of WINDOWS_REGISTRY_HIVE_LIST) {
+            try {
+                results.push(...enumerateValues(hive, key ? `${WINDOWS_REGISTRY_PATH}\\${key}` : WINDOWS_REGISTRY_PATH));
+            } catch (e) {
+                log.debug('Error accessing the registry', {hive, key}, e);
+            }
+        }
+        return results;
+    };
+
+    private getMacOSValue = (valueName: string): CFPrefValue | undefined => {
+        try {
+            return getCFPreferenceValue(valueName);
+        } catch (error) {
+            log.warn(`Nothing retrieved for '${valueName}'`, {error});
+            return undefined;
+        }
+    };
+}
+
+const policyConfigLoader = new PolicyConfigLoader();
+export default policyConfigLoader;

okrbest/okrbest-desktop

(cherry picked from commit 71cf1ac07289542694d1daf64e102ba0d92acb26)

src/common/config/policyConfigLoader.test.js
@@ -1,0 +1,234 @@
+// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2024-present OKR Best. All Rights Reserved.
+// See LICENSE.txt for license information.
+// Modified for OKR Best project.
+
+import {getPreferenceValue as getCFPreferenceValue} from 'cf-prefs';
+import {enumerateValues} from 'registry-js';
+
+import policyConfigLoader from 'common/config/policyConfigLoader';
+
+jest.mock('registry-js', () => {
+    return {
+        HKEY: {
+            HKEY_LOCAL_MACHINE: 'HKEY_LOCAL_MACHINE',
+            HKEY_CURRENT_USER: 'HKEY_CURRENT_USER',
+        },
+        enumerateValues: jest.fn().mockImplementation((hive, key) => {
+            if (hive === 'HKEY_LOCAL_MACHINE') {
+                if (key.endsWith('DefaultServerList')) {
+                    return [
+                        {name: 'server-lm-1', data: 'http://server-lm-1.com'},
+                        {name: 'server-lm-2', data: 'http://server-lm-2.com'},
+                    ];
+                }
+                if (key.includes('SOFTWARE\\Policies\\OKRBest')) {
+                    return [
+                        {name: 'EnableServerManagement', data: 1},
+                        {name: 'EnableAutoUpdater', data: 0},
+                    ];
+                }
+                return [];
+            }
+            if (hive === 'HKEY_CURRENT_USER') {
+                if (key.endsWith('DefaultServerList')) {
+                    return [
+                        {name: 'server-cu-1', data: 'http://server-cu-1.com'},
+                        {name: 'server-cu-2', data: 'http://server-cu-2.com'},
+                    ];
+                }
+                if (key.includes('SOFTWARE\\Policies\\OKRBest')) {
+                    return [
+                        {name: 'EnableServerManagement', data: 0},
+                        {name: 'EnableAutoUpdater', data: 1},
+                    ];
+                }
+                return [];
+            }
+            if (key.includes('Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize')) {
+                return [{name: 'AppsUseLightTheme', data: 1}];
+            }
+            return [];
+        }),
+    };
+});
+
+jest.mock('cf-prefs', () => ({
+    getPreferenceValue: jest.fn().mockImplementation((key) => {
+        if (key === 'DefaultServerList') {
+            return [
+                {name: 'server-a', url: 'https://a.com'},
+                {name: 'server-b', url: 'https://b.com'},
+            ];
+        }
+        if (key === 'EnableServerManagement') {
+            return true;
+        }
+        if (key === 'EnableAutoUpdater') {
+            return false;
+        }
+        return undefined;
+    }),
+}));
+
+describe('common/config/policyConfigLoader', () => {
+    let originalPlatform;
+
+    beforeEach(() => {
+        originalPlatform = process.platform;
+    });
+
+    afterEach(() => {
+        Object.defineProperty(process, 'platform', {value: originalPlatform});
+    });
+
+    describe('getPolicyConfig', () => {
+        describe('Windows', () => {
+            beforeEach(() => {
+                Object.defineProperty(process, 'platform', {value: 'win32'});
+            });
+
+            it('returns merged servers from both hives and CU overrides LM for booleans', () => {
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers).toHaveLength(4);
+                expect(data.servers).toContainEqual({name: 'server-lm-1', url: 'http://server-lm-1.com'});
+                expect(data.servers).toContainEqual({name: 'server-cu-1', url: 'http://server-cu-1.com'});
+                expect(data.enableServerManagement).toBe(false);
+                expect(data.enableUpdateNotifications).toBe(true);
+            });
+
+            it('handles undefined from one hive', () => {
+                enumerateValues.mockImplementation((hive, key) => {
+                    if (key.includes('OKRBest') && !key.includes('DefaultServerList')) {
+                        return hive === 'HKEY_LOCAL_MACHINE' ? [{name: 'EnableServerManagement', data: 1}] : [];
+                    }
+                    return [];
+                });
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.enableServerManagement).toBe(true);
+            });
+
+            it('handles registry error in one hive', () => {
+                enumerateValues.mockImplementation((hive, key) => {
+                    if (key.includes('OKRBest') && hive === 'HKEY_CURRENT_USER') {
+                        throw new Error('Registry access error');
+                    }
+                    if (key.includes('OKRBest') && !key.includes('DefaultServerList')) {
+                        return [{name: 'EnableServerManagement', data: 1}];
+                    }
+                    if (key.endsWith('DefaultServerList')) {
+                        return hive === 'HKEY_LOCAL_MACHINE' ? [{name: 's', data: 'http://s.com'}] : [];
+                    }
+                    return [];
+                });
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.enableServerManagement).toBe(true);
+                expect(data.servers).toHaveLength(1);
+            });
+
+            it('handles malformed server data', () => {
+                enumerateValues.mockImplementation((hive, key) => {
+                    if (key.endsWith('DefaultServerList')) {
+                        return [
+                            {name: 'server-1', data: null},
+                            {name: 'server-2', data: 'http://server-2.com'},
+                        ];
+                    }
+                    return [];
+                });
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers.length).toBeGreaterThanOrEqual(2);
+                expect(data.servers[0]).toEqual({name: 'server-1', url: null});
+                expect(data.servers[1]).toEqual({name: 'server-2', url: 'http://server-2.com'});
+            });
+
+            it('returns consistent data from multiple calls', () => {
+                const first = policyConfigLoader.getPolicyConfig();
+                const second = policyConfigLoader.getPolicyConfig();
+                expect(second.servers).toHaveLength(first.servers.length);
+                expect(second.enableServerManagement).toBe(first.enableServerManagement);
+            });
+        });
+
+        describe('macOS', () => {
+            beforeEach(() => {
+                Object.defineProperty(process, 'platform', {value: 'darwin'});
+            });
+
+            it('returns servers and booleans from managed preferences', () => {
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers).toEqual([
+                    {name: 'server-a', url: 'https://a.com'},
+                    {name: 'server-b', url: 'https://b.com'},
+                ]);
+                expect(data.enableServerManagement).toBe(true);
+                expect(data.enableUpdateNotifications).toBe(false);
+            });
+
+            it('returns empty servers and undefined booleans when no managed prefs', () => {
+                getCFPreferenceValue.mockImplementation(() => undefined);
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers).toEqual([]);
+                expect(data.enableServerManagement).toBeUndefined();
+                expect(data.enableUpdateNotifications).toBeUndefined();
+            });
+
+            it('handles cf-prefs throw gracefully', () => {
+                getCFPreferenceValue.mockImplementation((key) => {
+                    if (key === 'DefaultServerList') {
+                        throw new Error('cf-prefs error');
+                    }
+                    return undefined;
+                });
+                const data = policyConfigLoader.getPolicyConfig();
+                expect(data.servers).toEqual([]);
+            });
+        });
+    });
+
+    describe('getAppsUseLightTheme', () => {
+        it('returns true when AppsUseLightTheme is 1 on Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'win32'});
+            enumerateValues.mockImplementation((hive, key) => {
+                if (key.includes('Themes\\Personalize')) {
+                    return [{name: 'AppsUseLightTheme', data: 1}];
+                }
+                return [];
+            });
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(true);
+        });
+
+        it('returns false when AppsUseLightTheme is 0 on Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'win32'});
+            enumerateValues.mockImplementation((hive, key) => {
+                if (key.includes('Themes\\Personalize')) {
+                    return [{name: 'AppsUseLightTheme', data: 0}];
+                }
+                return [];
+            });
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(false);
+        });
+
+        it('returns true when registry key missing on Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'win32'});
+            enumerateValues.mockImplementation(() => []);
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(true);
+        });
+
+        it('returns true on non-Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'darwin'});
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(true);
+        });
+
+        it('returns true when registry access throws on Windows', () => {
+            Object.defineProperty(process, 'platform', {value: 'win32'});
+            enumerateValues.mockImplementation((hive, key) => {
+                if (key.includes('Themes\\Personalize')) {
+                    throw new Error('Registry error');
+                }
+                return [];
+            });
+            expect(policyConfigLoader.getAppsUseLightTheme()).toBe(true);
+        });
+    });
+});

src/common/config/policyConfigLoader.ts
@@ -1,0 +1,114 @@
+// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2024-present OKR Best. All Rights Reserved.
+// See LICENSE.txt for license information.
+// Modified for OKR Best project.
+
+import type {CFPrefValue} from 'cf-prefs';
+import {getPreferenceValue as getCFPreferenceValue} from 'cf-prefs';
+import type {RegistryValue} from 'registry-js';
+import {HKEY, enumerateValues} from 'registry-js';
+
+import {Logger} from 'common/log';
+
+import type {RegistryConfig as RegistryConfigType, Server} from 'types/config';
+
+const log = new Logger('PolicyConfigLoader');
+const WINDOWS_REGISTRY_PATH = 'SOFTWARE\\Policies\\OKRBest';
+const WINDOWS_REGISTRY_HIVE_LIST = [HKEY.HKEY_LOCAL_MACHINE, HKEY.HKEY_CURRENT_USER];
+
+export class PolicyConfigLoader {
+    getPolicyConfig = (): Partial<RegistryConfigType> => {
+        return {
+            servers: this.getServerList(),
+            enableServerManagement: this.getSingleBooleanValue('EnableServerManagement'),
+            enableUpdateNotifications: this.getSingleBooleanValue('EnableAutoUpdater'),
+        };
+    };
+
+    getAppsUseLightTheme = (): boolean => {
+        if (process.platform !== 'win32') {
+            return true;
+        }
+        try {
+            const results = enumerateValues(HKEY.HKEY_CURRENT_USER, 'Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize');
+            if (!results || results.length === 0) {
+                return true;
+            }
+            return results.find((item) => item.name === 'AppsUseLightTheme')?.data === 1;
+        } catch (error) {
+            log.debug('Error reading AppsUseLightTheme from registry', error);
+            return true;
+        }
+    };
+
+    private getServerList = (): Server[] => {
+        switch (process.platform) {
+        case 'win32':
+            return this.getWindowsServerList();
+        case 'darwin':
+            return this.getMacOSServerList();
+        default:
+            return [];
+        }
+    };
+
+    private getWindowsServerList = (): Server[] => {
+        const results = this.getWindowsValues('DefaultServerList');
+        return results.map((item) => ({name: item.name, url: item.data as string}));
+    };
+
+    private getMacOSServerList = (): Server[] => {
+        const results = this.getMacOSValue('DefaultServerList') as Array<Record<string, string>> | undefined;
+        return (results ?? []).map((item) => ({name: item.name, url: item.url}));
+    };
+
+    private getSingleBooleanValue = (valueName: string): boolean | undefined => {
+        switch (process.platform) {
+        case 'win32':
+            return this.getWindowsSingleBooleanValue(valueName);
+        case 'darwin':
+            return this.getMacOSSingleValue(valueName) as boolean | undefined;
+        default:
+            return false;
+        }
+    };
+
+    // Windows registry values are always numbers or strings, so we just check if the value is 1.
+    private getWindowsSingleBooleanValue = (valueName: string): boolean | undefined => {
+        const value = this.getWindowsSingleValue(valueName);
+        return value === undefined ? undefined : value === 1;
+    };
+
+    private getMacOSSingleValue = (valueName: string): Exclude<CFPrefValue, CFPrefValue[]> | undefined =>
+        this.getMacOSValue(valueName) as Exclude<CFPrefValue, CFPrefValue[]>;
+    private getWindowsSingleValue = (valueName: string) => {
+        // Windows provides 2 hives, so we need to get the value from both and return the first one
+        // The first one is the local machine hive, which should take priority
+        const results = this.getWindowsValues()?.filter((r) => r.name === valueName);
+        return results?.pop()?.data;
+    };
+
+    private getWindowsValues = (key?: string) => {
+        const results: RegistryValue[] = [];
+        for (const hive of WINDOWS_REGISTRY_HIVE_LIST) {
+            try {
+                results.push(...enumerateValues(hive, key ? `${WINDOWS_REGISTRY_PATH}\\${key}` : WINDOWS_REGISTRY_PATH));
+            } catch (e) {
+                log.debug('Error accessing the registry', {hive, key}, e);
+            }
+        }
+        return results;
+    };
+
+    private getMacOSValue = (valueName: string): CFPrefValue | undefined => {
+        try {
+            return getCFPreferenceValue(valueName);
+        } catch (error) {
+            log.warn(`Nothing retrieved for '${valueName}'`, {error});
+            return undefined;
+        }
+    };
+}
+
+const policyConfigLoader = new PolicyConfigLoader();
+export default policyConfigLoader;