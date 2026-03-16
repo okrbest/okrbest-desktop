6e3c702f861e8c0070a7008049c069848437f387
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2026-03-16 10:38:43 -0400

    [MM-67940] Suppress annoying error when theme is missing (#3731)
    (cherry picked from commit e4f51de3cf0e53ffdbbf48be23a0a9f33aade0a9)

--- src/renderer/utils.ts
+++ src/renderer/utils.ts
@@ -100,6 +100,9 @@ const printVersion = () => {
 };
 
 const setTheme = (theme: Theme) => {
+    if (!theme) {
+        return;
+    }
     document.body.style.setProperty('--sidebar-bg', theme.sidebarBg);
     document.body.style.setProperty('--sidebar-bg-rgb', toRgbValues(theme.sidebarBg));
     document.body.style.setProperty('--sidebar-text', theme.sidebarText);
