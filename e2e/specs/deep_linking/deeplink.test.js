017ba31318f8751d48bdf7dc4aa57f5a6e69d766
yasser khan <attitude3cena.yf@gmail.com>
2026-03-16 12:28:59 +0530

    feat(e2e): Run E2E test on GPO/MDM configured github runners (#3708)
    (cherry picked from commit 710643c473108c47bcfa9294842ce684cee7e28f)

--- e2e/specs/deep_linking/deeplink.test.js
+++ e2e/specs/deep_linking/deeplink.test.js
@@ -10,7 +10,7 @@ const env = require('../../modules/environment');
 const {asyncSleep} = require('../../modules/utils');
 
 describe('application', function desc() {
-    this.timeout(30000);
+    this.timeout(120000);
 
     const config = env.demoConfig;
 
